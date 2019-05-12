f1ed4dd8f3aa4d987506e5fd534ac8c824b25a56
Julian Heng <19473701@student.curtin.edu.au>
2019-05-12 19:43:28 +0800

    Store program options in a collection for reference

--- src/RouteTracker/controller/RouteTracker.java
+++ src/RouteTracker/controller/RouteTracker.java
@@ -15,15 +15,21 @@ public class RouteTracker
     public static void main(String[] args)
     {
         Map<String,Route> routes = new HashMap<>();
+        List<Option> options = new ArrayList<>();
 
         GeoUtils utils = new GeoUtils();
         UserInterface ui = new ConsoleUI();
         Menu menu = new Menu(ui);
-        menu.addOption(new GetRouteData(utils, routes));
-        menu.addOption(new ClearRoute(routes));
-        menu.addOption(new PrintAllRoute(routes));
-        menu.addOption(new PrintRouteDetailed(routes));
-        menu.addOption(new PrintAllRouteDetailed(routes));
+
+        // Make options collection so that we can reference it to chain(?)
+        // option's doOption() method
+        options.add(new GetRouteData(utils, routes));
+        options.add(new ClearRoute(routes));
+        options.add(new PrintAllRoute(routes));
+        options.add(new PrintRouteDetailed(routes));
+        options.add(new PrintAllRouteDetailed(routes));
+
+        options.forEach((v)->menu.addOption(v));
 
         // Main program loop
         do
